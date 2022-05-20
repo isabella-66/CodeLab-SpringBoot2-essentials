edf0a9e54cbde27029ce7b3e11afb3b92589d1a5
isabella-66 <amasprog@gmail.com>
2022-05-19 23:44:15 -0300

    Configurando action 'save', através do método POST

--- src/main/java/br/com/ifsp/springboot2/service/AnimeService.java
+++ src/main/java/br/com/ifsp/springboot2/service/AnimeService.java
@@ -7,21 +7,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> anime = List.of(new Anime (1L, "DBZ"), new Anime (2L, "Berserk"));
+    private static List<Anime> animes;
 
+    static { //porque ainda não tem bd
+        animes = new ArrayList<>(List.of(new Anime (1L, "DBZ"), new Anime (2L, "Berserk")));
+    }
     //private final AnimeRepository animeRepository
     public List<Anime> listAll() {
-        return anime;
+        return animes;
     }
 
     public Anime findById(Long id) {
-        return anime.stream()
+        return animes.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(8, 100000));
+        animes.add(anime);
+        return anime;
     }
 }
