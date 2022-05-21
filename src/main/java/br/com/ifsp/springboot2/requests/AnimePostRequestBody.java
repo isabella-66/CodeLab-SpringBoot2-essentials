a18a94b27c082c7c557f86e346217c13f45d11f4
isabella-66 <amasprog@gmail.com>
2022-05-21 19:58:39 -0300

    Configurando validação de campo vazio, através da dependência 'starter validation'

--- src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,11 @@ package br.com.ifsp.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The name cannot be empty")
     private String name;
 }
