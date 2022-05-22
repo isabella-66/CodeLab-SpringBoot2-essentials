b4e57779b06e4e34cd9497f28f707d23487eb626
isabella-66 <amasprog@gmail.com>
2022-05-22 19:59:02 -0300

    Aplicando Spring Security com autenticação de memória do usuário

---
+++ src/test/java/br/com/ifsp/springboot2/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package br.com.ifsp.springboot2.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //todas as requisições, URLs, que tem o controller, precisarão passar por autenticação básica
+        http.authorizeRequests() //o que quer proteger
+                .anyRequest() //todas as requisições
+                .authenticated() //requisições autenticadas
+                .and()
+                .httpBasic(); //forma de autenticação
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //criação do usuário em memória e tipo de criptografia para sua senha
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password Encoded: {}", passwordEncoder.encode("test"));
+        auth.inMemoryAuthentication()
+                .withUser("isabella")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("devdojo")
+                .password(passwordEncoder.encode("academy"))
+                .roles("USER");
+    }
+}
