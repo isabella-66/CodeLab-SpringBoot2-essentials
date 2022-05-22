55a6d5bc2e14999f54e10189d7fdf9521a97d78f
isabella-66 <amasprog@gmail.com>
2022-05-22 19:12:49 -0300

    Incluindo o Maven Profile para separar os profiles e executar os testes de integração em diferentes ciclos

--- src/test/java/br/com/ifsp/springboot2/Springboot2EssentialsApplicationTests.java
+++ src/test/java/br/com/ifsp/springboot2/Springboot2EssentialsApplicationTests.java
@@ -3,10 +3,10 @@ package br.com.ifsp.springboot2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Springboot2EssentialsApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
