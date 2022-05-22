ffc1778333952be9062184a3658ffa7781dde8b4
isabella-66 <amasprog@gmail.com>
2022-05-21 21:03:07 -0300

    Aprimorando configuração de validação, através do Handler

--- src/main/java/br/com/ifsp/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/br/com/ifsp/springboot2/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package br.com.ifsp.springboot2.handler;
 
 import br.com.ifsp.springboot2.exception.BadRequestException;
 import br.com.ifsp.springboot2.exception.BadRequestExceptionDetails;
+import br.com.ifsp.springboot2.exception.ValidationExceptionDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -21,6 +26,27 @@ public class RestExceptionHandler {
                         .title("Bad Request Exception, Check the Documentation")
                         .details(bre.getMessage())
                         .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException (
+            MethodArgumentNotValidException exception){
+        //todos os campos com erro
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        //mensagem dos campos
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/br/com/ifsp/springboot2/requests/AnimePostRequestBody.java
@@ -1,6 +1,7 @@
 package br.com.ifsp.springboot2.requests;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
